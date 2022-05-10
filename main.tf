ff77d6eae796f52e632892d61d02d742d224e289
swapnats1 <swapnats933@gmail.com>
2022-05-10 12:18:37 -0400

    updated file

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws"{
 region = "us-east-2"
 }
 resource "aws_instance" "ec2create" {
-ami = "ami-0fa49cc9dc8d62c84"
-subnet_id = "subnet-0ec05be64e942f4f4"
+ami = "ami-xxxxxxxxxxxx"
+subnet_id = "subnet-xxxxxxxxxxxx"
 instance_type = "t2.micro"
 associate_public_ip_address = "true"
-key_name = "swapna"
+key_name = "xxxxx"
 
 
 tags = {
